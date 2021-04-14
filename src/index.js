e95dc937b29becee626f8ea36185f83d939d2ec0
samuel <samuelalmaoherrera@gmail.com>
2021-04-14 10:31:32 -0300

    updated general performance and added linters.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import profile from '../assets/images/ppphoto.png';
+import icon from '../assets/images/icon.png';
+
+document.getElementById('profile').setAttribute('src', profile);
+document.getElementById('smallIcon').setAttribute('href', icon);
+document.getElementById('bigIcon').setAttribute('href', icon);
